cbdbdbd9abfa5cb6db1b2ab84408e925033b3782
mahendra1290 <mahendra.suthar.1290@gmail.com>
2018-12-30 23:20:44 +0530

    snake working awesome

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pygame, time, random, snake, food
+import pygame, os, time, random, snake, food
 
-width  = 490
 pygame.init()
-height = 490
+width  = 600
+height = 600
 feed_color = "asd"
 food_infom = 0
 tempss = 0
-game_cont = false
+game_cont = False
 colors = {
     'blue'   : (0, 0, 255),
     'red'    : (255, 0, 0),
@@ -18,8 +18,35 @@ colors = {
     'cyan'   : (156, 222, 254)
 }
 win    = pygame.display.set_mode((width, height))
-snakee = snake.Snake(7, (10, 130, 30), win, (width, height))
-foodss  = food.Food(7, 490)
+head = {'w':pygame.image.load(os.path.join('snake_head', 'head_W.png')).convert_alpha(),
+        'e':pygame.image.load(os.path.join('snake_head', 'head_E.png')).convert_alpha(),
+        'n':pygame.image.load(os.path.join('snake_head', 'head_N.png')).convert_alpha(),
+        's':pygame.image.load(os.path.join('snake_head', 'head_S.png')).convert_alpha()
+        }
+
+tail = {'w':pygame.image.load(os.path.join('snake_tail', 'tail_W.png')).convert_alpha(),
+        'e':pygame.image.load(os.path.join('snake_tail', 'tail_E.png')).convert_alpha(),
+        'n':pygame.image.load(os.path.join('snake_tail', 'tail_N.png')).convert_alpha(),
+        's':pygame.image.load(os.path.join('snake_tail', 'tail_S.png')).convert_alpha()
+        }
+
+body = {'e':pygame.image.load(os.path.join('snake_body', 'body_X.png')).convert_alpha(),
+        'w':pygame.image.load(os.path.join('snake_body', 'body_X.png')).convert_alpha(),
+        'n':pygame.image.load(os.path.join('snake_body', 'body_Y.png')).convert_alpha(),
+        's':pygame.image.load(os.path.join('snake_body', 'body_Y.png')).convert_alpha(),
+        'en':pygame.image.load(os.path.join('snake_body', 'body_EN.png')).convert_alpha(),
+        'es':pygame.image.load(os.path.join('snake_body', 'body_ES.png')).convert_alpha(),
+        'wn':pygame.image.load(os.path.join('snake_body', 'body_WN.png')).convert_alpha(),
+        'ws':pygame.image.load(os.path.join('snake_body', 'body_WS.png')).convert_alpha(),
+        'ne':pygame.image.load(os.path.join('snake_body', 'body_NE.png')).convert_alpha(),
+        'nw':pygame.image.load(os.path.join('snake_body', 'body_NW.png')).convert_alpha(),
+        'se':pygame.image.load(os.path.join('snake_body', 'body_SE.png')).convert_alpha(),
+        'sw':pygame.image.load(os.path.join('snake_body', 'body_SW.png')).convert_alpha()
+        }
+
+whole_snake = {'head':head, 'body':body, 'tail':tail}
+snakee = snake.Snake(win, (width, height), 15, whole_snake)
+foodss  = food.Food(15, 600)
 foods_1 = True
 not_touch = False
 food_infom = 0
@@ -30,17 +57,16 @@ clock = pygame.time.Clock()
 
 def food_cordinate(food_check):
     for i in range(1, len(food_check)):
-        food_x = random.randint(0, 69)
-        food_y = random.randint(0, 69)
-        food_x *= 7
-        food_y *= 7
+        food_x = random.randint(0, 39)
+        food_y = random.randint(0, 39)
+        food_x *= 15
+        food_y *= 15
         if (food_x in range(food_check[i-1]['x'], food_check[i]['x'])) \
         and (food_y in range(food_check[i-1]['y'], food_check[i]['y'])):    
             continue
         else:
             feed_color = random.choice(list(colors.keys()))
             return {'x': food_x, 'y':food_y, 'col':colors[feed_color]}
-
 while run:
     while game_cont:
         for event in pygame.event.get():
@@ -49,13 +75,11 @@ while run:
                     game_cont = False
 
     win.fill((10, 10, 10))
+
     if foods_1:
         food_check = snakee.get_snake()
-        food_infom = food_cordinate(food_check)
+        food_infom = {'x': 60, 'y':60, 'col':colors['blue']}
         foods_1 = False
-
-    if snakee.self_collision():
-        game_cont = True
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -63,37 +87,37 @@ while run:
                 game_cont = True
                 break
             if event.key == pygame.K_UP:
-                snakee.move_up()
+                snakee.turn_up_down(down=False)
                 break
              
             if event.key == pygame.K_DOWN:
-                snakee.move_down()
+                snakee.turn_up_down()
                 break
              
             if event.key == pygame.K_LEFT:
-                snakee.move_left()
+                snakee.turn_left_right(right=False)
                 break
              
             if event.key == pygame.K_RIGHT:
-                snakee.move_right()
+                snakee.turn_left_right()
                 break
              
         if event.type == pygame.QUIT:
             run = False
             break
     snakee.snake_move()
-    if snakee.get_pos()['x'] == food_infom['x']  and snakee.get_pos()['y'] == food_infom['y']:
-        tempss = food_infom.copy()
-        foods_1 = True
-    if foods_1 or not_touch:
-        not_touch = True
-        if snakee.get_pos(head=False)['x'] == tempss['x'] and snakee.get_pos(head=False)['y'] == tempss['y']:
-            not_touch = False
-            snakee.update_snake(tempss['col'])
-        pygame.draw.rect(win ,tempss['col'],[tempss['x'], tempss['y'], 7, 7])
-    foodss.update_food(food_infom['col'], win, food_infom['x'], food_infom['y'])       
+    # if snakee.get_pos()['x'] == food_infom['x']  and snakee.get_pos()['y'] == food_infom['y']:
+    #     tempss = food_infom.copy()
+    #     foods_1 = True
+    # if foods_1 or not_touch:
+    #     not_touch = True
+    #     if snakee.get_pos(head=False)['x'] == tempss['x'] and snakee.get_pos(head=False)['y'] == tempss['y']:
+    #         not_touch = False
+    #         snakee.update_snake(tempss['col'])
+    #     pygame.draw.rect(win ,tempss['col'],[tempss['x'], tempss['y'], 7, 7])
+    #foodss.update_food(food_infom['col'], win, food_infom['x'], food_infom['y'])      
     pygame.display.update()
-    clock.tick(15)
+    clock.tick(10)
 pygame.quit()
 
 
