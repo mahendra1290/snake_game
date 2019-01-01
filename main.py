cdcb33a1f79b510e5fadd31cbb268f4e54d7e54a
mahendra1290 <mahendra.suthar.1290@gmail.com>
2019-01-02 01:41:11 +0530

    continue and game over option screen left

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import pygame, os, time, random, snake, food
-print(os.listdir('snake_body') )
+import pygame, os, time, random, snake, food, font
+
+FPS = 10
+main_direct = 'snakes'
 pygame.init()
 width  = 600
 height = 600
+black = (0, 0, 0)
+score = 0
 feed_color = "asd"
 food_infom = 0
 tempss = 0
-game_cont = False
+game_cont  = False
+game_start = True
+game_over  = False
 colors = {
     'blue'   : (0, 0, 255),
     'red'    : (255, 0, 0),
@@ -18,34 +24,30 @@ colors = {
     'cyan'   : (156, 222, 254)
 }
 win    = pygame.display.set_mode((width, height))
-pygame.display.set_icon(pygame.image.load(os.path.join('snake_head', 'head_W.png')).convert_alpha())
-head = {'w':pygame.image.load(os.path.join('snake_head', 'head_W.png')).convert_alpha(),
-        'e':pygame.image.load(os.path.join('snake_head', 'head_E.png')).convert_alpha(),
-        'n':pygame.image.load(os.path.join('snake_head', 'head_N.png')).convert_alpha(),
-        's':pygame.image.load(os.path.join('snake_head', 'head_S.png')).convert_alpha()
-        }
+pygame.display.set_icon(pygame.image.load(os.path.join('snakes\snake_1\snake_head', 'head_W.png')).convert_alpha())
 
-tail = {'w':pygame.image.load(os.path.join('snake_tail', 'tail_W.png')).convert_alpha(),
-        'e':pygame.image.load(os.path.join('snake_tail', 'tail_E.png')).convert_alpha(),
-        'n':pygame.image.load(os.path.join('snake_tail', 'tail_N.png')).convert_alpha(),
-        's':pygame.image.load(os.path.join('snake_tail', 'tail_S.png')).convert_alpha()
-        }
+def snake_loader(mainpath, snake_no):
+    snake = {'head':{}, 'body':{}, 'tail':{}}
+    snake_direc = {
+                'head' : ['e', 'n', 's', 'w'],
+                'tail' : ['e', 'n', 's', 'w'],
+                'body' : ['e', 'en', 'es', 'n', 'ne', 'nw', 's', 'se', 'sw', 'w', 'wn', 'ws']
+    }
+    snakes_list = os.listdir(mainpath)
+    temp = os.path.join(mainpath, snakes_list[snake_no])
+    snake_part = os.listdir(temp)
+    print(snake_part)
+    snake_key = list(snake.keys())
+    snake_key.sort()
+    for i, v in zip(snake_part, snake_key):
+        part_path = os.path.join(temp, i)
+        part = os.listdir(part_path)
+        for j in range(len(part)):
+            abs_path = os.path.join(part_path, part[j])
+            snake[v].update({snake_direc[v][j]:pygame.image.load(abs_path).convert_alpha()})
+    return snake
 
-body = {'e':pygame.image.load(os.path.join('snake_body', 'body_X.png')).convert_alpha(),
-        'w':pygame.image.load(os.path.join('snake_body', 'body_X.png')).convert_alpha(),
-        'n':pygame.image.load(os.path.join('snake_body', 'body_Y.png')).convert_alpha(),
-        's':pygame.image.load(os.path.join('snake_body', 'body_Y.png')).convert_alpha(),
-        'en':pygame.image.load(os.path.join('snake_body', 'body_EN.png')).convert_alpha(),
-        'es':pygame.image.load(os.path.join('snake_body', 'body_ES.png')).convert_alpha(),
-        'wn':pygame.image.load(os.path.join('snake_body', 'body_WN.png')).convert_alpha(),
-        'ws':pygame.image.load(os.path.join('snake_body', 'body_WS.png')).convert_alpha(),
-        'ne':pygame.image.load(os.path.join('snake_body', 'body_NE.png')).convert_alpha(),
-        'nw':pygame.image.load(os.path.join('snake_body', 'body_NW.png')).convert_alpha(),
-        'se':pygame.image.load(os.path.join('snake_body', 'body_SE.png')).convert_alpha(),
-        'sw':pygame.image.load(os.path.join('snake_body', 'body_SW.png')).convert_alpha()
-        }
-
-whole_snake = {'head':head, 'body':body, 'tail':tail}
+whole_snake = snake_loader(main_direct, 2)
 snakee = snake.Snake(win, (width, height), 15, whole_snake)
 foodss  = food.Food(15, 600)
 foods_1 = True
@@ -55,7 +57,8 @@ food_infom = 0
 pygame.display.set_caption("Feed_the_Snake")
 run = True
 clock = pygame.time.Clock()
-
+start = pygame.image.load('temp\start.png').convert_alpha()
+game = pygame.image.load('temp\game.fw.png').convert_alpha()
 def food_cordinate(food_check):
     not_get_food = True
     while not_get_food:
@@ -73,8 +76,26 @@ def food_cordinate(food_check):
             feed_color = random.choice(list(colors.keys()))
             not_get_food = False
             return {'x': food_x, 'y':food_y, 'col':colors[feed_color]}
+
+main_text = font.Text('temp\comic.ttf', 30, win)
+flash_text  = font.Flash_text('temp\comic.ttf', 40, win)
 while run:
-    win.fill((10, 10, 10))
+    while game_start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                game_start = False
+                break
+            #z = pygame.mouse.get_pos()
+            #print(z)
+        win.blit(start, (0,0))
+        if flash_text.draw("Play", colors['orange'], colors['red'], (115, 325)):
+            game_start = False
+            break
+        flash_text.draw("Options", colors['blue'], colors['white'], (80, 460))
+        pygame.display.update()
+        clock.tick(FPS)
+    win.blit(game, (0, 0))
     while game_cont:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -112,6 +133,7 @@ while run:
         foods_1 = False
 
     if snakee.is_collision(food_detail['x'], food_detail['y']):
+        score += 1
         snakee.update_snake()
         foods_1 = True
 
@@ -119,8 +141,9 @@ while run:
     snakee.snake_move() 
     if snakee.self_collision():
         game_cont = True
+    main_text.draw("Score "+str(score), black, (5, 5))
     pygame.display.update()
-    clock.tick(10)
+    clock.tick(FPS)
 pygame.quit()
 
 
